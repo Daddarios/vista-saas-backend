63990226d280b4f746e1e34cd4c8bf728dcbb9fc
Daddarios <onurgoekhanbicer@gmail.com>
2026-05-12 21:03:49 +0200

    fixed

---
+++ Migrations/20260512185827_UpdateMandantIdDefaultValue.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vista.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMandantIdDefaultValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
